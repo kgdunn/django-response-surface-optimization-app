f8e3309ccdcede30b63ad8aafc131517121afd32
Kevin Dunn <kgdunn@gmail.com>
2015-10-26 16:05:06 +0100

    Added checking for -Inf, +Inf and NaN

--- rsm/urls.py
+++ rsm/urls.py
@@ -4,8 +4,12 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.show_all_systems, name='show_all_systems'),
-    
+
     # ex: /system/short_name/
-    url(r'^system/(?P<short_name_slug>[-\w]+)/$', views.show_one_system, 
-        name='show_one_system'),    
+    url(r'^system/(?P<short_name_slug>[-\w]+)/$', views.show_one_system,
+        name='show_one_system'),
+
+    # ex: /system/short_name/run
+    url(r'^system/(?P<short_name_slug>[-\w]+)/run$',
+        views.run_experiment, name='run-experiment'),
 ]
