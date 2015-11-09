5b4ae8133a8ba50b09277b4cc99dd1d563841323
Kevin Dunn <kgdunn@gmail.com>
2015-11-09 16:24:47 -0500

    Moved the static file location to a different place; better matches production environment.

--- rsmproject/settings.py
+++ rsmproject/settings.py
@@ -93,7 +93,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-STATIC_URL = '/static/'
+STATIC_URL = '/media/'
 
 try:
     from local_settings import *
